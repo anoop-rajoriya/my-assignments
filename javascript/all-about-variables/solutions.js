5a05028bbc99ba777e1b76be6b07d44ff2444baf
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2026-05-18 10:30:43 +0530

    Add modules for variable declarations, naming rules, scopes, and hoisting with starter code and test cases

---
+++ javascript/all-about-variables/solutions.js
@@ -0,0 +1,11 @@
+// Module 1: Variable Declarations (`var`, `let`, and `const`)
+
+
+// Module 2: Variables Naming Rules
+
+
+// Module 3: Variable Scopes (Block, Function, and Global)
+
+
+// Module 4: Variable Hoisting
+
